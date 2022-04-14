90670bc37e64ac2c9bf5362cffc7dae0bc622b04
OjChi <ojasa.chitre@gmail.com>
2022-04-14 13:31:12 +0530

    First commit

---
+++ majorProj/charts/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+#import dash functions here
+# from dashboard.dash_apps.finished_apps import simpleexample
+#end dash functions
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homeView, name="home"),
+    path('crypto/', views.cryptoView, name="crypto"),
+]
---
+++ majorProj/charts/views.py
@@ -0,0 +1,194 @@
+from django.shortcuts import render, redirect
+
+#most dependencies and imports made in functions.py to avoid clutter
+from .functions import *
+
+# Create your views here.
+
+def homeView(request):
+
+
+    api_key = 'YX9741BHQFXIYA0B'
+
+    stock = 'PLTR'
+
+    api_key = 'YX9741BHQFXIYA0B'
+    period= 60
+
+    ts = TimeSeries(key=api_key, output_format='pandas',)
+    data_ts, meta_data_ts = ts.get_intraday(stock, interval='1min', outputsize='compact')
+
+    ti = TechIndicators(key=api_key, output_format='pandas')
+    data_ti, meta_data_ti  = ti.get_rsi(stock, interval='1min', time_period=period, series_type='close')
+
+    ts_df = data_ts
+    ti_df = data_ti
+
+    #Fundamentals
+    payload = {'function': 'OVERVIEW', 'symbol': 'PLTR', 'apikey': 'YX9741BHQFXIYA0B'}
+    r = requests.get('https://www.alphavantage.co/query', params=payload)
+    r = r.json()
+
+
+    #plotly graph
+    def candlestick():
+        figure = go.Figure(
+            data = [
+                    go.Candlestick(
+                      x = ts_df.index,
+                      high = ts_df['2. high'],
+                      low = ts_df['3. low'],
+                      open = ts_df['1. open'],
+                      close = ts_df['4. close'],
+                    )
+                  ]
+        )
+
+        candlestick_div = plot(figure, output_type='div')
+        return candlestick_div
+
+
+    sector = r['Sector']
+    marketcap = r['MarketCapitalization']
+    peratio = r['PERatio']
+    yearhigh = r['52WeekHigh']
+    yearlow = r['52WeekLow']
+    eps = r['EPS']
+
+
+
+    timeseries = ts_df.to_dict(orient='records')
+
+    closingprice = []
+    for k in timeseries:
+      closingprice.append(k['4. close'])
+
+    lowprice = []
+    for k in timeseries:
+      closingprice.append(k['3. low'])
+
+    highprice = []
+    for k in timeseries:
+      closingprice.append(k['2. high'])
+
+    openprice = []
+    for k in timeseries:
+      closingprice.append(k['1. open'])
+
+    pricedata = {
+        'close': [closingprice],
+        'open': [openprice],
+        'high': [highprice],
+        'low': [lowprice],
+    }
+
+    #miscellaneous stuff
+    day = datetime.datetime.now()
+    day = day.strftime("%A")
+
+    def human_format(num):
+        magnitude = 0
+        while abs(num) >= 1000:
+            magnitude += 1
+            num /= 1000.0
+        # add more suffixes if you need them
+        return '%.2f%s' % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
+
+    marketcap = int(marketcap)
+    marketcap = human_format(marketcap)
+
+    closingprice = closingprice[0:15]
+
+
+    context = {
+        'sector': sector,
+        'marketcap': marketcap,
+        'peratio': peratio,
+        'yearhigh': yearhigh,
+        'yearlow': yearlow,
+        'eps': eps,
+        'closingprice': closingprice,
+        'openprice': openprice,
+        'highprice': highprice,
+        'lowprice': lowprice,
+        'pricedata': pricedata,
+        'timeseries': timeseries,
+        'stock': stock,
+        'day': day,
+        'candlestick': candlestick(),
+    }
+
+    context={}
+    return render(request, 'dashboard/index.html', context)
+    
+
+
+def homeView(request):
+    if request.method == 'POST':
+        symbol = request.POST.get('symbol')
+        return redirect('crypto/')
+
+    context={
+
+    }
+    return render(request, 'dashboard/index.html', context)
+
+
+
+def cryptoView(request):
+
+    if request.method == 'POST':
+        symbol = request.POST.get('symbol')
+        symbol = symbol.upper()
+    else:
+        symbol = 'BTCUSD'
+
+    data = spotquote(symbol)
+    pricedata = pricechange(symbol)
+    moredata = pricechange(symbol)
+
+    actual_df = pd.read_csv(f'C:/Users/ojasa/Documents/BE/Major Project/Proj/majorProj/combined_0imputed_HDFC_Bank_withDate.csv')
+    ts_df = actual_df
+
+    #get a fricken df
+    # ts_df = candles(symbol)
+    #PlotlyGraph
+    def candlestick():
+        figure = go.Figure(
+            data = [
+                    go.Candlestick(
+                      # x = ts_df.index,
+                      x = ts_df['Date'],
+                      high = ts_df['High'],
+                      low = ts_df['Low'],
+                      open = ts_df['Open'],
+                      close = ts_df['Close'],
+                    )
+                  ]
+        )
+
+        candlestick_div = plot(figure, output_type='div')
+        return candlestick_div
+    #endPlotlyGraph
+    # percentchange = pricedata['priceChangePercent']
+    # buyers = pricedata['askQty']
+    # sellers = pricedata['bidQty']
+
+    # eth = pricechange(symbol='ETHUSD')
+    # btc = pricechange(symbol="BTCUSD")
+    # ltc = pricechange(symbol="LTCUSD")
+
+
+
+    context={
+    # 'moredata': moredata,
+    # 'eth': eth,
+    # 'btc': btc,
+    # 'ltc': ltc,
+    # 'percentchange': percentchange,
+    # 'buyers': buyers,
+    # 'sellers': sellers,
+    'data': data,
+    'candlestick': candlestick(),
+    }
+    return render(request, 'dashboard/crypto.html', context)
